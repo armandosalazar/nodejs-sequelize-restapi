13e4a56d27b50af205ab1fd3cde32e1ccc727066
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2022-07-29 14:41:52 -0500

    Update location of config file

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-import config from '../../config';
+import config from '../config';
 
 const sequelize = new Sequelize(config.DB, config.DB_USER, config.DB_PASSWORD, {
   host: config.DB_HOST,
